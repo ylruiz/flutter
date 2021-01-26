5c7daf1cb6c95cfe0efdcf92c8b18751ad195a6a
Yunet Luis Ruiz <yunet.lruiz94@gmail.com>
2021-01-26 21:10:15 +0100

    Tutorial number 1(https://www.youtube.com/watch?v=GLSG_Wh_YWc)

---
+++ flutter_course/lib/product_manager.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+import 'products.dart';
+
+class ProductManager extends StatefulWidget {
+  final String startingProduct;
+
+  ProductManager({this.startingProduct}) {
+    print('[ProductManager Widget] Constructor');
+  }
+
+  @override
+  State<StatefulWidget> createState() {
+    print('[ProductManager Widget] ceateState()');
+    return _ProductManagerState();
+  }
+}
+
+class _ProductManagerState extends State<ProductManager> {
+  List<String> _products = [];
+
+  @override
+  void initState() {
+    print('[ProductManager State] initState()');
+    _products.add(widget.startingProduct);
+    super.initState();
+  }
+
+  @override
+  void didUpdateWidget(covariant ProductManager oldWidget) {
+    print('[ProductManager State] didUpdateWidget()');
+    super.didUpdateWidget(oldWidget);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    print('[ProductManager State] build()');
+    return Column(children: [
+      Container(
+        margin: EdgeInsets.all(5.0),
+        child: RaisedButton(
+            color: Theme.of(context).primaryColor,
+            child: Text('Add Product'),
+            onPressed: () {
+              setState(() => _products.add('Advanced Food Tester'));
+            }),
+      ),
+      Products(_products)
+    ]);
+  }
+}
