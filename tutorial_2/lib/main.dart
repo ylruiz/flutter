c29a34ae314fbf46c4e95688dffc4420f043ac60
Yunet Luis Ruiz <yunet.lruiz94@gmail.com>
2021-01-27 16:02:31 +0100

    Tutorial #2 - Using Image widget

--- tutorial_2/lib/main.dart
+++ tutorial_2/lib/main.dart
@@ -5,9 +5,7 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Home()
-    );
+    return MaterialApp(home: Home());
   }
 }
 
@@ -15,28 +13,20 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('my first app'),
-          centerTitle: true,
-          backgroundColor: Colors.red[600],
-        ),
-        body: Center(
-          child: Text(
-            'hello ninjas!',
-            style: TextStyle(
-              fontSize: 20.0,
-              fontWeight: FontWeight.bold,
-              letterSpacing: 2.0,
-              color: Colors.grey[600],
-              fontFamily: 'IndieFlower',
-            ),
-          ),
-        ),
-        floatingActionButton: FloatingActionButton(
-          child: Text('click'),
-          backgroundColor: Colors.red[600],
-          onPressed: () {},
-        ),
-      );
+      appBar: AppBar(
+        title: Text('my first app'),
+        centerTitle: true,
+        backgroundColor: Colors.red[600],
+      ),
+      body: Center(
+        child: Image.network(
+            'https://images.unsplash.com/photo-1472552944129-b035e9ea3744?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTV8fHNreXxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Text('click'),
+        backgroundColor: Colors.red[600],
+        onPressed: () {},
+      ),
+    );
   }
 }
