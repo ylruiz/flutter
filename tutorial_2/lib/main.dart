8f42b2e3b2d08949578b7e2859694eaaa61b7424
Yunet Luis Ruiz <yunet.lruiz94@gmail.com>
2021-01-28 10:17:22 +0100

    Tutorial #2 - Finish The Net Ninja project

--- tutorial_2/lib/main.dart
+++ tutorial_2/lib/main.dart
@@ -9,7 +9,15 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+
+  int ninjaLevel = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,6 +27,13 @@ class Home extends StatelessWidget {
         centerTitle: true,
         backgroundColor: Colors.grey[850],
         elevation: 0,
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        backgroundColor: Colors.grey[800],
+        onPressed: (){
+          setState(() => ninjaLevel++);
+        },
       ),
       body: Padding(
         padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0),
@@ -62,7 +77,7 @@ class Home extends StatelessWidget {
             ),
             SizedBox(height: 10.0),
             Text(
-              '8',
+              '$ninjaLevel',
               style: TextStyle(
                 color: Colors.amberAccent[200],
                 letterSpacing: 2,
@@ -73,19 +88,14 @@ class Home extends StatelessWidget {
             SizedBox(height: 30.0),
             Row(
               children: <Widget>[
-                Icon(
-                  Icons.email, 
-                  color: Colors.grey[400]
-                ),
+                Icon(Icons.email, color: Colors.grey[400]),
                 SizedBox(width: 10.0),
-                Text(
-                  'chon.li@thenetninja.co.uk',
-                  style: TextStyle(
-                    color: Colors.grey[400],
-                    fontSize: 18.0,
-                    letterSpacing: 1.0,
-                  )
-                ),
+                Text('chon.li@thenetninja.co.uk',
+                    style: TextStyle(
+                      color: Colors.grey[400],
+                      fontSize: 18.0,
+                      letterSpacing: 1.0,
+                    )),
               ],
             ),
           ],
