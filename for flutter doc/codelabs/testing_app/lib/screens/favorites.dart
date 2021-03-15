d55a35e9e4735756f560f5f78f29f9423afb45ef
Yunet Luis Ruiz <yunet.lruiz94@gmail.com>
2021-03-15 17:23:44 +0100

    Adding visual and project structure

---
+++ for flutter doc/codelabs/testing_app/lib/screens/favorites.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:testing_app/models/favorites.dart';
+
+class FavoritesPage extends StatelessWidget {
+  static String routeName = '/favorites_page';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Favorites')),
+      body: Consumer<Favorites>(
+          builder: (context, value, child) => ListView.builder(
+                itemCount: value.items.length,
+                padding: EdgeInsets.symmetric(vertical: 16),
+                itemBuilder: (context, index) =>
+                    FavoriteItemTile(itemNo: value.items[index]),
+              )),
+    );
+  }
+}
+
+class FavoriteItemTile extends StatelessWidget {
+  final int itemNo;
+
+  const FavoriteItemTile({Key key, this.itemNo}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(8.0),
+      child: ListTile(
+        leading: CircleAvatar(
+          backgroundColor: Colors.primaries[itemNo % Colors.primaries.length],
+        ),
+        title: Text(
+          'Item $itemNo',
+          key: Key('favorites_text_$itemNo'),
+        ),
+        trailing: IconButton(
+          key: Key('icon_$itemNo'),
+          icon: Icon(Icons.close),
+          onPressed: () {
+            Provider.of<Favorites>(context, listen: false).delete(itemNo);
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(
+                content: Text('Deleted from favorites'),
+                duration: Duration(seconds: 1),
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
